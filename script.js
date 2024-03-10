3d27417d4c09231844deb6a2e9a8b765ed150cad
Bharath <56076642+Bharath09876@users.noreply.github.com>
2024-03-10 06:22:54 -0700

    landing page home

---
+++ script.js
@@ -0,0 +1,9 @@
+const navLinks = document.querySelectorAll(".nav_item a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", function() {
+    navLinks.forEach(otherLink => otherLink.classList.remove("active"));  // Remove active class from all links
+    this.classList.add("active");  // Add active class to the clicked link
+  });
+});
+alert("hello")
