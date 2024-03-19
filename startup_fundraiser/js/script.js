4c149fa66a5e94c749b52c2ce757da37a8f71f88
bharath-arch <76623645+bharath-arch@users.noreply.github.com>
2024-03-19 01:53:26 -0700

    Delete startup_fundraiser directory

--- startup_fundraiser/js/script.js
+++
@@ -1,9 +0,0 @@
-// const navLinks = document.querySelectorAll(".nav_item a");
-
-// navLinks.forEach(link => {
-//   link.addEventListener("click", function() {
-//     navLinks.forEach(otherLink => otherLink.classList.remove("active"));  // Remove active class from all links
-//     this.classList.add("active");  // Add active class to the clicked link
-//   });
-// });
-// alert("hello")
